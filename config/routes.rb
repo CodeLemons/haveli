2caf8bec3bf2ab92830900e4a8b1ba0c216f599a
Ali-Hamza Asam <asamalihamza@gmail.com>
2022-12-19 22:16:19 +0000

    chagnes

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,19 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-
+  get '/homepage', to: "pages#home"
+  resources :users, only: [:new, :create]
+  resources :customers, only: [:index, :show, :new, :create, :edit, :update] do
+    resources :orders, only: [:index, :show, :new, :create]
+  end
+  resources :restaurants, only: [:index, :show, :new, :create, :edit, :update] do
+    resources :menus, only: [:index, :show, :new, :create, :edit, :update]
+  end
+  resources :menus, only: [:index, :show] do
+    resources :menu_items, only: [:index, :show]
+  end
+  resources :orders, only: [] do
+    resources :order_items, only: [:index, :show, :new, :create, :edit, :update]
+  end
+  resources :reservations, only: [:index, :show, :new, :create, :edit, :update]
 end
