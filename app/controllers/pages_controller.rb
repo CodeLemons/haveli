24fe366a065230fbd030d455173f5e2bbc4e84c6
Ali-Hamza Asam <asamalihamza@gmail.com>
2023-01-06 17:47:53 +0000

    fixed master removed contact and reservation dependencies and forms from view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,13 +6,9 @@ class PagesController < ApplicationController
     I18n.locale = params[:locale]
     cookies[:menu_id] = params[:menu_id] if params[:menu_id]
     @menu = Menu.all
-    # id = params[:menu_id] || Menu.first.id
     cookies[:menu_id] ? id = cookies[:menu_id] : id = Menu.first.id
     @menu_items = MenuItem.includes(photo_attachment: :blob).includes(:text_translations).includes(:shopping_cart_items).search_by_id(id)
-    # @menu_items = MenuItem.where(menu_id: id)
     @gallery = Gallery.includes(image_attachment: :blob).all
-    @contact = Contact.new
-    @reservation = Reservation.new
     @shopping_cart = ShoppingCart.new
   end
 end
--- app/models/contact.rb
+++
@@ -1,7 +0,0 @@
-class Contact < ApplicationRecord
-    validates :first_name, presence: true
-    validates :last_name, presence: true
-    validates :email, presence: true
-    validates :phone, presence: true
-    validates :message, presence: true
-end
