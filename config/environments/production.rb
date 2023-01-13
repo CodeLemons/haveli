e96ee9c0b6406f3e8c76628e989e7f0c23ae6055
Ali-Hamza Asam <asamalihamza@gmail.com>
2023-01-13 16:36:20 +0000

    added mailer config settings to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,9 +1,20 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: "http://TODO_PUT_YOUR_DOMAIN_HERE" }
+  config.action_mailer.default_url_options = { host: "https://haveli-ro.herokuapp.com/" }
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "gmail.com",
+    user_name: "asamalihamza@gmail.com",
+    password: "yxvsknawmfdmwduo",
+    authentication: "plain",
+    enable_starttls_auto: true
+  }
+  
   # Code is not reloaded between requests.
   config.cache_classes = true
 
