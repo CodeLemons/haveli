693e56ca5c6e3b1093ed6209a243d33a50565962
Ali-Hamza Asam <asamalihamza@gmail.com>
2023-01-13 14:39:37 +0000

    fixed some migrations

--- db/migrate/20221224142027_create_orders.rb
+++ db/migrate/20221224142027_create_orders.rb
@@ -1,7 +1,6 @@
 class CreateOrders < ActiveRecord::Migration[7.0]
   def change
     create_table :orders do |t|
-      t.references :customer, null: false, foreign_key: true
       t.references :restaurant, null: false, foreign_key: true
       t.datetime :order_date
       t.time :order_time
