dea36472740cc03333861dae60390ac23d3f25e6
Ali-Hamza Asam <asamalihamza@gmail.com>
2023-01-15 12:51:44 +0000

    Attached broll video and logo to Restaurant Modelfor dynamic fetching

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -6,4 +6,6 @@ class Restaurant < ApplicationRecord
   validates :address, presence: true
   validates :phone, presence: true
   validates :website, presence: true, format: { with: URI.regexp(%w[http https]) }
+  has_one_attached :video
+  has_one_attached :logo
 end
