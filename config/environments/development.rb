2b017345ab04c1eb735fd5ad27c514c54ca9ebdd
Ali-Hamza Asam <asamalihamza@gmail.com>
2023-01-27 11:02:38 +0000

    set up actionmailer delivery method

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,10 +38,17 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :test
   config.action_mailer.smtp_settings = {
-    
-  }
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            ENV['GMAIL_USERNAME'],
+    password:             ENV['GMAIL_PASSWORD'],
+    authentication:       'plain',
+    enable_starttls_auto: true,
+    open_timeout:         5,
+    read_timeout:         5 }
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
